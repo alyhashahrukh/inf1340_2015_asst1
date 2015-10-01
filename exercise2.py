1e4759a5f28f231daee3f31d3f8c19675b2aeee8
alyhashahrukh <alyha.shahrukh@mail.utoronto.ca>
2015-10-01 10:42:29 -0400

    Added assignment 1 outline as comment.

--- exercise2.py
+++ exercise2.py
@@ -13,6 +13,16 @@ __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 
+# Assignment Outline
+# Write a program that determines the name of a shape from its number of sides. Read the number of
+# sides from the user and then report the appropriate name as part of a meaningful message. Your program
+# should support shapes with anywhere from 3 up to (and including) 10 sides. If the input something other
+# than the numbers 3 to 10 then your program should display 'Error'. Your code should be easily readable.
+# The function should include comments, proper variable naming and produce messages in case of errors.
+# Add your code and test cases to the starter file, exercise2.py, that has been provided. Do not
+# change the names of the functions in the file.
+
+
 def name_that_shape():
     """
     For a given number of sides in a regular polygon, returns the shape name
